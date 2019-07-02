6bcdcfeace3e6b45ea82bbea5473fdc4b175e2e2
patrickclery <patrick.clery@gmail.com>
2019-07-02 06:08:48 -0700

    profile service object now returns struct

--- spec/tinder/profile_spec.rb
+++ spec/tinder/profile_spec.rb
@@ -4,18 +4,14 @@ require 'webmock'
 RSpec.describe Tinder::Client do
   include WebMock::API
 
-  subject { described_class }
+  subject { described_class.tap { |client| client.api_token = api_token } }
+  let(:api_token) { "12a3bc45-a123-123a-1a23-1234abc4de5f" }
+  let!(:json) { File.read("spec/fixtures/profile.json") }
 
   it { should respond_to(:get_active_profile) }
   it { should respond_to(:get_active_profile).with(0).arguments }
 
-  let(:api_token) { "eyJhbGciOiJIUzI1NiJ9.MTc3ODk5MDk4MDM.5q4R0H08rE0Dd9KgxMPp6jcTfIBLCXgEuVZfC9znJTE" }
-  let!(:json) do
-    File.read("spec/fixtures/profile.json")
-  end
-
   before do
-    subject.api_token = api_token
     stub_request(:get, "http://api.gotinder.com/v2/profile?include=account,boost,email_settings,instagram,likes,notifications,plus_control,products,purchase,spotify,super_likes,tinder_u,travel,tutorials,user")
       .to_return(body: json)
   end
