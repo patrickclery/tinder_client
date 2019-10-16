2f4d71038c85258b5c1067d4ca9d7b0a35dd2b84
patrickclery <patrick.clery@gmail.com>
2019-10-16 02:41:19 -0700

    URL was incorrect

--- lib/tinder/client.rb
+++ lib/tinder/client.rb
@@ -9,9 +9,10 @@ module Tinder
     # Always prefer V2 endpoints as the API is less buggy than V1
     BASE_URI  = 'https://api.gotinder.com/v2'
     ENDPOINTS = {
-      request_code: "/auth/sms/send?auth_type=sms",
-      login:        "/auth/login/sms",
-      validate:     "/auth/sms/validate?auth_type=sms"
+      request_code:    "/auth/sms/send?auth_type=sms",
+      login:           "/auth/login/sms",
+      validate:        "/auth/sms/validate?auth_type=sms",
+      recommendations: "/recs/core"
     }
 
     attr_accessor :api_token
@@ -73,6 +74,13 @@ module Tinder
         "User-agent":   "Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)",
         "Accept":       "application/json",
         "X-Auth-Token": @api_token
+      }
+      {
+        "User-Agent":     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0",
+        "app-version":    "1020900",
+        "tinder-version": "2.9.0",
+        "platform":       "web",
+        "X-Auth-Token":   api_token
       }
     end
 
