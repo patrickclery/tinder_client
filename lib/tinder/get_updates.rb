a3c12a249e878569d1221eb13a1f0a4bbb67c655
patrickclery <patrick.clery@gmail.com>
2019-10-20 17:42:01 -0700

    :recycle: Refactoring code.

--- lib/tinder/get_updates.rb
+++ lib/tinder/get_updates.rb
@@ -15,69 +15,69 @@ module Tinder
 
   class Updates < Dry::Struct
 
-    attribute :blocks, Dry::Types['array'].of(Dry::Types['string'])
-    attribute :deleted_lists, Dry::Types['array']
-    attribute :goingout, Dry::Types['array']
-    attribute :harassing_messages, Dry::Types['array']
-    attribute :inbox, Dry::Types['array'] do
-      attribute :_id, Dry::Types['string']
-      attribute :match_id, Dry::Types['string']
-      attribute :sent_date, Dry::Types['string']
-      attribute :message, Dry::Types['string']
-      attribute :to, Dry::Types['string']
-      attribute :from, Dry::Types['string']
-      attribute :created_date, Dry::Types['string']
-      attribute :timestamp, Dry::Types['coercible.string']
+    attribute :blocks, Types.array.of(Types.string)
+    attribute :deleted_lists, Types.array
+    attribute :goingout, Types.array
+    attribute :harassing_messages, Types.array
+    attribute :inbox, Types.array do
+      attribute :_id, Types.string
+      attribute :match_id, Types.string
+      attribute :sent_date, Types.string
+      attribute :message, Types.string
+      attribute :to, Types.string
+      attribute :from, Types.string
+      attribute :created_date, Types.string
+      attribute :timestamp, Types.send('coercible.string')
     end
-    attribute :poll_interval, Dry::Types['hash']
-    attribute :liked_messages, Dry::Types['array'] do
-      attribute :message_id, Dry::Types['string']
-      attribute :updated_at, Dry::Types['string']
-      attribute :liker_id, Dry::Types['string']
-      attribute :match_id, Dry::Types['string']
-      attribute :is_liked, Dry::Types['bool']
+    attribute :poll_interval, Types.hash
+    attribute :liked_messages, Types.array do
+      attribute :message_id, Types.string
+      attribute :updated_at, Types.string
+      attribute :liker_id, Types.string
+      attribute :match_id, Types.string
+      attribute :is_liked, Types.bool
     end
-    attribute :lists, Dry::Types['array']
-    attribute :matches, Dry::Types['array'] do
-      attribute :_id, Dry::Types['string']
-      attribute :closed, Dry::Types['bool']
-      attribute :common_friend_count, Dry::Types['integer']
-      attribute :common_like_count, Dry::Types['integer']
-      attribute :created_date, Dry::Types['string']
-      attribute :dead, Dry::Types['bool']
-      attribute :following, Dry::Types['bool']
-      attribute :following_moments, Dry::Types['bool']
-      attribute :id, Dry::Types['string']
-      attribute :is_boost_match, Dry::Types['bool']
-      attribute :is_fast_match, Dry::Types['bool']
-      attribute :is_super_like, Dry::Types['bool']
-      attribute :last_activity_date, Dry::Types['string']
-      attribute :message_count, Dry::Types['integer']
-      attribute :messages, Dry::Types['array'] do
-        attribute :_id, Dry::Types['string']
-        attribute :match_id, Dry::Types['string']
-        attribute :sent_date, Dry::Types['string']
-        attribute :message, Dry::Types['string']
-        attribute :to, Dry::Types['string']
-        attribute :from, Dry::Types['string']
-        attribute :created_date, Dry::Types['string']
-        attribute :timestamp, Dry::Types['coercible.string']
+    attribute :lists, Types.array
+    attribute :matches, Types.array do
+      attribute :_id, Types.string
+      attribute :closed, Types.bool
+      attribute :common_friend_count, Types.integer
+      attribute :common_like_count, Types.integer
+      attribute :created_date, Types.string
+      attribute :dead, Types.bool
+      attribute :following, Types.bool
+      attribute :following_moments, Types.bool
+      attribute :id, Types.string
+      attribute :is_boost_match, Types.bool
+      attribute :is_fast_match, Types.bool
+      attribute :is_super_like, Types.bool
+      attribute :last_activity_date, Types.string
+      attribute :message_count, Types.integer
+      attribute :messages, Types.array do
+        attribute :_id, Types.string
+        attribute :match_id, Types.string
+        attribute :sent_date, Types.string
+        attribute :message, Types.string
+        attribute :to, Types.string
+        attribute :from, Types.string
+        attribute :created_date, Types.string
+        attribute :timestamp, Types.send('coercible.string')
       end
-      attribute :muted, Dry::Types['bool']
-      attribute :participants, Dry::Types['array']
-      attribute :pending, Dry::Types['bool']
+      attribute :muted, Types.bool
+      attribute :participants, Types.array
+      attribute :pending, Types.bool
       attribute :person do
-        attribute? :bio, Dry::Types['string']
-        attribute :birth_date, Dry::Types['string']
-        attribute :gender, Dry::Types['integer']
-        attribute :name, Dry::Types['string']
-        attribute :ping_time, Dry::Types['string']
-        attribute :photos, Dry::Types['array']
+        attribute? :bio, Types.string
+        attribute :birth_date, Types.string
+        attribute :gender, Types.integer
+        attribute :name, Types.string
+        attribute :ping_time, Types.string
+        attribute :photos, Types.array
       end
-      attribute :readreceipt, Dry::Types['hash']
-      attribute :seen, Dry::Types['hash']
+      attribute :readreceipt, Types.hash
+      attribute :seen, Types.hash
     end
-    attribute :squads, Dry::Types['array']
+    attribute :squads, Types.array
 
   end
 end
